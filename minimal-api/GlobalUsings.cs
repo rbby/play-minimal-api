2dc725561b569b1f10f90449a4253966abe0e76f
Robby <robbypot@gmail.com>
2021-12-06 21:12:28 +0100

    added things

---
+++ minimal-api/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using Microsoft.OpenApi.Models;
+global using MinimalApi.Services;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using System.Security.Claims;
---
+++ minimal-api/readme.md
@@ -0,0 +1,5 @@
+﻿
+
+## resources: 
+- https://anthonygiretti.com/2021/08/12/asp-net-core-6-working-with-minimal-apis/
+- https://sandrino.dev/blog/aspnet-core-5-jwt-authorization
